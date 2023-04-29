194b2c4f2a29bf2695d4e2e5c2e78bd3561e2fc8
HBA114 <hbasriayhaner114@gmail.com>
2023-04-29 18:06:31 +0300

    Updated shell command

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MainApp extends StatelessWidget {
                         // YoutubeDownloadTest or youtubeMP3
                         if (filePath.value != "" && downloadPath.value != "") {
                           await shell.run(
-                              "./YoutubeDownloadTest ${filePath.value} ${downloadPath.value}");
+                              "YoutubeDownloadTest ${filePath.value} ${downloadPath.value}");
                           isFinished.value = true;
                           filePath.value = "";
                           downloadPath.value = "";
